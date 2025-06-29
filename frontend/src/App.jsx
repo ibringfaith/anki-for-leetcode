656c3e0964f88ea490b533d002b897caa7f17682
Katherine Awad <ka1014@princeton.edu>
2025-06-29 00:09:34 -0400

    Replaced all vite/react logos references with the anki4leetcode logo. Played around with changing standard vite app text.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import prodLogo from './assets/anki4leetcode_logo.svg'
 import './App.css'
 
 function App() {
@@ -9,20 +8,17 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a>
+          <img src={prodLogo} className="h-82 w-82 logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Anki4Leetcode</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <p className="pt-6">
+          You should lock in.
         </p>
       </div>
       <p className="read-the-docs">
